2af7411f1ce7804a08342bebc76a0d283813165c
Armin <hueneburg.armin@gmail.com>
2014-11-20 04:09:03 +0100

    first stable version

---
+++ SplittingM4bToMp3/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace SplittingM4bToMp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Contains("-x"))
+            {
+                foreach (var arg in args)
+                {
+                    if (arg != "-x")
+                    {
+                        ExtractInformation("\"" + arg + "\"");
+                    }
+                }
+            }
+            else
+            {
+                if (!Directory.Exists("output"))
+                {
+                    Directory.CreateDirectory("output");
+                }
+                foreach (var path in args)
+                {
+                    string path1 = path;
+                    var thread = new Thread(() =>
+                    {
+                        Console.WriteLine("Started " + path1);
+                        var file = ExtractInformation("\"" + path1 + "\"");
+                        CutFiles(path1, file);
+                        Console.WriteLine("Finished " + path1);
+                    });
+                    thread.Start();
+                }
+            }
+        }
+
+        private static void CutFiles(string path, string name)
+        {
+            const string arguments = "{2} -i \"{1}\" -acodec libmp3lame -ss {0} {3}";
+            using (var reader = new StreamReader(name))
+            {
+                var line1 = reader.ReadLine().Split(';');
+                string[] line2;
+                string l;
+                while ((l = reader.ReadLine()) != null)
+                {
+                    line2 = l.Split(';');
+                    var temp = string.Format(arguments, line1[0], path, "-t " + line2[0], "output/\"" + line1[1] + ".mp3\"");
+                    var p = new Process
+                    {
+                        StartInfo = {Arguments = temp, FileName = "/usr/bin/ffmpeg", UseShellExecute = false, RedirectStandardOutput = true}
+                    };
+                    var thread = new Thread(() => p.Start());
+                    thread.Start();
+                    line1 = line2;
+                }
+                var t = string.Format(arguments, line1[0], path, "", "output/\"" + line1[1] + ".mp3\"");
+                var pro = new Process
+                {
+                    StartInfo = { Arguments = t, FileName = "/usr/bin/ffmpeg", UseShellExecute = false }
+                };
+                var th = new Thread(() => pro.Start());
+                th.Start();
+            }
+        }
+
+        private static string ExtractInformation(string file)
+        {
+            var p = new Process
+            {
+                StartInfo = {UseShellExecute = false, RedirectStandardOutput = true, FileName = "/usr/bin/mediainfo", Arguments = file}
+            };
+            p.Start();
+            var stream = p.StandardOutput;
+            string line;
+            var inMenuOne = false;
+            var output = "";
+            while ((line = stream.ReadLine()) != null)
+            {
+                if (inMenuOne && line != "")
+                {
+                    output += line + "\n";
+                } 
+                else if (!inMenuOne && line.StartsWith("Menu"))
+                {
+                    inMenuOne = true;
+                }
+                else if (inMenuOne && line == "")
+                {
+                    break;
+                }
+            }
+            {
+                var ori = output.Replace("  ", " ");
+                while (ori != output)
+                {
+                    output = ori;
+                    ori = output.Replace("  ", " ");
+                }
+            }
+            output = output.Replace(" : ", ";");
+            var name = file.Split('/')[file.Split('/').Length - 1];
+            name = name.Substring(0, name.Length - 1);
+            using (var writer = new StreamWriter(name + ".names"))
+            {
+                writer.Write(output);
+            }
+            return name + ".names";
+        }
+    }
+}
---
+++ SplittingM4bToMp3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SplittingM4bToMp3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SplittingM4bToMp3")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("396f8435-f1c8-4b5d-b735-857c7c4dca85")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
